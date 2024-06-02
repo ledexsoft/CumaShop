9dcfed8c7d1f5c7325c8658a1bcaa804c69a1fd1
Ledexsoft <ledexsoftrgmail.com>
2024-06-02 15:41:54 -0400

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   assets/bd/conectar-api.js
    	new file:   assets/bd/productos-mas-vendidos.js
    	modified:   home-grocery.html

---
+++ assets/bd/conectar-api.js
@@ -0,0 +1,7 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+
+// Inicializa Supabase
+const supabase = createClient(
+  'https://ogtbjqbvfmnlvkucqutj.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ndGJqcWJ2Zm1ubHZrdWNxdXRqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxNzM1NTYyMSwiZXhwIjoyMDMyOTMxNjIxfQ.1uP3YbHw5HSLkoPOVZOjeYhgYZ88fK20S6RWQR_QIIE'
+);
