fc0e5d7f0f9a6eaf3cb2944cb41f86dd438baa9c
Ledexsoft <ledexsoftrgmail.com>
2024-06-23 14:36:55 -0400

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   assets/bd/detalles-del-producto.js
    	modified:   shop-product-grocery.html

--- assets/bd/detalles-del-producto.js
+++ assets/bd/detalles-del-producto.js
@@ -9,33 +9,36 @@ const supabase = createClient(
 
 
 
-document.addEventListener('DOMContentLoaded', async function() {
-  // Obtén el ID del producto almacenado en sessionStorage
-  var idProducto = sessionStorage.getItem('sessionStorageCategoria');
 
-  // Realiza una consulta a Supabase para obtener los detalles del producto
-  try {
-    const { data, error } = await supabase
-      .from('Productos de Tienda de Comestibles') // Asegúrate de que este sea el nombre correcto de tu tabla
-      .select('*')
-      .eq('id', idProducto) // Usa el ID del producto para filtrar los resultados
-      .single(); // Suponiendo que solo esperas un resultado
+  // Reemplaza 'idProducto' con el ID real del producto que deseas mostrar
+  const idProducto = sessionStorage.getItem('sessionStorageCategoria');
 
-    if (error) throw error;
-
-    // Si la consulta fue exitosa, inserta los valores en las etiquetas HTML
-    if (data) {
-      document.getElementById('descripcionProductoCategoria').textContent = data.Categoría;
-      document.getElementById('descripcionProductoNombre').textContent = data.Nombre;
-      document.getElementById('descripcionProductoDescripción').textContent = data.Descripción;
-      document.getElementById('descripcionProductoPrecio').textContent = `$${data.PrecioCUP}`;
-      document.getElementById('descripcionProductoStock').textContent = data.Stock;
-      document.getElementById('descripcionProductoCantidad').textContent = data.Cantidad;
-      document.getElementById('descripcionProductoImg1').src = data.Foto1;
-      document.getElementById('descripcionProductoImg2').src = data.Foto2;
-      document.getElementById('descripcionProductoImg3').src = data.Foto3;; // Asegúrate de que 'Cantidad' es un campo en tu tabla
+  document.addEventListener('DOMContentLoaded', async () => {
+    try {
+      const { data, error } = await supabase
+        .from('Productos de Tienda de Comestibles') // Asegúrate de que este sea el nombre correcto de tu tabla
+        .select('*')
+        .eq('id', idProducto) // Usa el ID del producto para filtrar los resultados
+        .single(); // Suponiendo que solo esperas un resultado
+  
+      if (error) throw error;
+  
+      // Si la consulta fue exitosa, inserta los valores en las etiquetas HTML
+      if (data) {
+        document.getElementById('descripcionProductoCategoria').textContent = data.Tipo;
+        document.getElementById('descripcionProductoNombre').textContent = data.Nombre;
+        document.getElementById('descripcionProductoCantidad').textContent = data.Cantidad;
+        document.getElementById('descripcionProductoFoto').src = data.Foto1;
+        document.getElementById('descripcionProductoDescripción').textContent = data.Descripcion;
+        document.getElementById('descripcionProductoPrecio').textContent = `$${data.PrecioCUP}`;
+        document.getElementById('descripcionProductoFoto2').src = data.Foto2;
+        document.getElementById('descripcionProductoFoto3').src = data.Foto3;
+        document.getElementById('descripcionProductoStock').textContent = data.Stock;
+        
+      }
+    } catch (error) {
+      console.error('Error fetching product details:', error);
     }
-  } catch (error) {
-    console.error('Error fetching product details:', error);
-  }
-});
+  });
+
+
